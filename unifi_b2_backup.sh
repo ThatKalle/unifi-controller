1c763e9d5260b18ade85aaf38e5c34b7cbd0b49e
Kalle Lilja <desethsc@gmail.com>
2017-10-15 16:26:07 +0200

    Update unifi_b2_backup.sh

--- unifi_b2_backup.sh
+++ unifi_b2_backup.sh
@@ -3,11 +3,19 @@ cd /tmp
 # Backup /var/lib/unifi/backup
 TIMESTMP=$(date +'%Y%m%d_%H%M%S')
 tar -zcvf backup.$TIMESTMP.tar.gz /var/lib/unifi/backup
-/usr/sbin/rclone copy /tmp/backup.$TIMESTMP.tar.gz B2_UNIFI.CONTROLLER.NAME:BUCKETNAME
+/usr/sbin/rclone copy /tmp/backup.$TIMESTMP.tar.gz B2_UNIFI.CONTROLLER.NAME:BUCKETNAME-unifi
 rm backup.$TIMESTMP.tar.gz
 
 # Backup /var/lib/unifi/sites
 TIMESTMP=$(date +'%Y%m%d_%H%M%S')
 tar -zcvf sites.$TIMESTMP.tar.gz /var/lib/unifi/sites
-/usr/sbin/rclone copy /tmp/sites.$TIMESTMP.tar.gz B2_UNIFI.CONTROLLER.NAME:BUCKETNAME
+/usr/sbin/rclone copy /tmp/sites.$TIMESTMP.tar.gz B2_UNIFI.CONTROLLER.NAME:BUCKETNAME-unifi
 rm sites.$TIMESTMP.tar.gz
+
+# Remote Cleanup
+# Only run Cleanup if there are data present in bucket newer than 6 weeks == previous backups successful
+if [[ $(/usr/sbin/rclone ls B2_UNIFI.CONTROLLER.NAME:BUCKETNAME-unifi --max-age 6w) ]]; then
+# Delete everything older than 6 weeks
+/usr/sbin/rclone delete B2_UNIFI.CONTROLLER.NAME:BUCKETNAME-unifi --min-age 6w
+/usr/sbin/rclone cleanup B2_UNIFI.CONTROLLER.NAME:BUCKETNAME-unifi --min-age 8w
+fi
